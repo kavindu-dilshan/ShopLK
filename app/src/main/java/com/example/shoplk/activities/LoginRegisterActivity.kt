0d98551edc8682f211e93315c8811c606be3db5c
Kavindu Dilshan <kavindudilshan84@gmail.com>
2024-01-30 18:26:36 +0530

    initial commit

---
+++ app/src/main/java/com/example/shoplk/activities/LoginRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shoplk.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shoplk.R
+
+class LoginRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+}
