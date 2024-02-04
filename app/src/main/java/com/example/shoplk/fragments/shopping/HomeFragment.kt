c99ca8a0bc90d46df1b78d3e6aa5a56d6de09fef
Kavindu Dilshan <kavindudilshan84@gmail.com>
2024-02-04 23:58:02 +0530

    add bottom navigation

---
+++ app/src/main/java/com/example/shoplk/fragments/shopping/HomeFragment.kt
@@ -0,0 +1,7 @@
+package com.example.shoplk.fragments.shopping
+
+import androidx.fragment.app.Fragment
+import com.example.shoplk.R
+
+class HomeFragment: Fragment(R.layout.fragment_home) {
+}
---
+++ app/src/main/java/com/example/shoplk/fragments/shopping/ProfileFragment.kt
@@ -0,0 +1,7 @@
+package com.example.shoplk.fragments.shopping
+
+import androidx.fragment.app.Fragment
+import com.example.shoplk.R
+
+class ProfileFragment: Fragment(R.layout.fragment_profile) {
+}
